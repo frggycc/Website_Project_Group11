57c80394ba6bb0c8f286931f65eb0085d65436ab
SoupIsSoup <jacjac511@yahoo.com>
2025-04-21 17:46:08 -0700

    update to search bar

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def shopping_cart():
 
     return render_template("shopping_cart.html", cart_items=cart_items, total_price=total_price)
 
-@app.route('/search-Bar', methods=["GET", "POST"])
+@app.route('/search-bar', methods=["GET", "POST"])
 def search_bar():
     search_query = request.args.get("search", "").lower()
     print("Search Query:", search_query)
